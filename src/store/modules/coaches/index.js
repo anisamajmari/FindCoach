048456833062ac3f1ad5fe1a0be06d5250b0310a
Anisa Majmari <anisamajmari@gmail.com>
2023-07-12 13:31:11 +0200

    adding vuex and coach data

---
+++ src/store/modules/coaches/index.js
@@ -0,0 +1,34 @@
+import mutations from './mutations.js';
+import actions from './actions.js';
+import getters from './getters.js';
+
+export default {
+  namespaced: true,
+  state() {
+    return {
+      coaches: [
+        {
+          id: 'c1',
+          firstName: 'Anisa',
+          lastName: 'Majmari',
+          areas: ['frontend'],
+          description:
+            "I'm Anisa and I am a junior front end developer. I am learning Vue!",
+          hourlyRate: 25,
+        },
+        {
+          id: 'c2',
+          firstName: 'Julie',
+          lastName: 'Jones',
+          areas: ['frontend', 'career', 'backend'],
+          description:
+            'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+          hourlyRate: 30,
+        },
+      ],
+    };
+  },
+  mutations,
+  actions,
+  getters,
+};
