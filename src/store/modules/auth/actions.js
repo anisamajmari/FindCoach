f2c48f9dbbe5cf22533166a340da6f8bb9b6a121
Anisa Majmari <anisamajmari@gmail.com>
2023-07-24 19:57:21 +0200

    auto login

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -3,7 +3,29 @@ import { SIGNUP_URL, LOGIN_URL, API_KEY } from '../../../config.js';
 export default {
   //LOGIN
   async login(context, payload) {
-    const response = await fetch(`${LOGIN_URL}?key=${API_KEY}`, {
+    context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    });
+  },
+
+  //SIGNUP
+  async signup(context, payload) {
+    context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url = LOGIN_URL;
+
+    if (mode === 'signup') {
+      url = SIGNUP_URL;
+    }
+
+    const response = await fetch(`${url}?key=${API_KEY}`, {
       method: 'POST',
       body: JSON.stringify({
         email: payload.email,
@@ -21,7 +43,8 @@ export default {
       throw error;
     }
 
-    console.log(responseData);
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
 
     context.commit('setUser', {
       token: responseData.idToken,
@@ -30,33 +53,17 @@ export default {
     });
   },
 
-  //SIGNUP
-  async signup(context, payload) {
-    const response = await fetch(`${SIGNUP_URL}?key=${API_KEY}`, {
-      method: 'POST',
-      body: JSON.stringify({
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true,
-      }),
-    });
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
 
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      const error = new Error(
-        responseData.message || 'Failed to authenticate.'
-      );
-      throw error;
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokenExpiration: null,
+      });
     }
-
-    console.log(responseData);
-
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
-    });
   },
 
   //LOGOUT
