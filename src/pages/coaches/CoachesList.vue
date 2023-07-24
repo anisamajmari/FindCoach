10fe20d9a84f633b464324ae2f22f96fb3221f3a
Anisa Majmari <anisamajmari@gmail.com>
2023-07-24 11:54:05 +0200

    fetching coach data

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,7 +6,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
         <base-button v-if="!isCoach" link to="/register"
           >Register as Coach</base-button
         >
@@ -66,9 +66,17 @@ export default {
     },
   },
 
+  created() {
+    this.loadCoaches();
+  },
+
   methods: {
     setFilters(updatedFilters) {
       this.activFilters = updatedFilters;
+    },
+
+    loadCoaches() {
+      this.$store.dispatch('coaches/loadCoaches');
     },
   },
 };
