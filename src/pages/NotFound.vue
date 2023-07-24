c4133363731af04e48014ef500d42ff0564cd4d8
Anisa Majmari <anisamajmari@gmail.com>
2023-07-24 13:59:54 +0200

    not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,1 +1,11 @@
-<template>Not Found</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not found. Maybe check out all our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
