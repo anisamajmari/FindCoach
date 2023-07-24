617faebeda9f2d129350346872a48e1b2e29b873
Anisa Majmari <anisamajmari@gmail.com>
2023-07-24 16:02:40 +0200

    login action

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,7 +1,36 @@
-import { SIGNUP_URL, API_KEY } from '../../../config.js';
+import { SIGNUP_URL, LOGIN_URL, API_KEY } from '../../../config.js';
 
 export default {
-  login() {},
+  //LOGIN
+  async login(context, payload) {
+    const response = await fetch(`${LOGIN_URL}?key=${API_KEY}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to authenticate.'
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+
+  //SIGNUP
   async signup(context, payload) {
     const response = await fetch(`${SIGNUP_URL}?key=${API_KEY}`, {
       method: 'POST',
